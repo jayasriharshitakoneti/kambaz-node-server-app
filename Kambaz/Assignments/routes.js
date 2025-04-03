3fede4700cb23661ccd45507d8ee0e472dd182df
jayasriharshitakoneti <harshitakoneti@gmail.com>
2025-04-03 07:05:20 -0400

    a5 completed

---
+++ Kambaz/Assignments/routes.js
@@ -0,0 +1,19 @@
+import * as assignmentsDao from "./dao.js";
+
+export default function AssignmentRoutes(app) {
+  app.delete("/api/assignments/:assignmentId", async (req, res) => {
+    const { assignmentId } = req.params;
+    const status = await assignmentsDao.deleteAssignment(assignmentId);
+    res.send(status);
+  });
+
+  app.put("/api/assignments/:assignmentId", async (req, res) => {
+    const { assignmentId } = req.params;
+    const updatedAssignment = req.body;
+    const status = await assignmentsDao.updateAssignment(
+      assignmentId,
+      updatedAssignment
+    );
+    res.send(status);
+  });
+}
