3ef89091cf8067976d6b03f642c5b21587d7c6a2
jayasriharshitakoneti <harshitakoneti@gmail.com>
2025-04-18 03:55:16 -0400

    done until 6.4.3

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -8,6 +8,11 @@ export default function CourseRoutes(app) {
     const courses = await dao.findAllCourses();
     res.send(courses);
   });
+  app.post("/api/courses", async (req, res) => {
+    const course = await dao.createCourse(req.body);
+    res.json(course);
+  });
+
   app.delete("/api/courses/:courseId", async (req, res) => {
     const { courseId } = req.params;
     const status = await dao.deleteCourse(courseId);
